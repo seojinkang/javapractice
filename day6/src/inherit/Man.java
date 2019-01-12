54e28e25babbababb7f6072e2cf85c51714ee351
seojinkang <46391724+seojinkang@users.noreply.github.com>
2019-01-12 13:08:37 +0900

    Add files via upload

---
+++ day6/src/inherit/Man.java
@@ -0,0 +1,10 @@
+package inherit;
+
+public class Man extends Person{
+	//정적특징
+	int speed;
+	//동적특징- run
+	public void run() {
+		System.out.println("달리다.");
+	}
+}
---
+++ day6/src/inherit/MyHome.java
@@ -0,0 +1,19 @@
+package inherit;
+
+public class MyHome {
+
+	public static void main(String[] args) {
+		SuperMan superMan = new SuperMan();
+		superMan.name = "Å¬¶óÅ©";
+		superMan.age = 100;
+
+		superMan.speed = 100;
+		superMan.high = 1000;
+		
+		superMan.smile();
+		superMan.run();
+		superMan.fly();
+		
+	}
+
+}
